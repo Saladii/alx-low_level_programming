41523e60ddc4919dfea18c0b6b04d5c65f488414
root <selehadinseid1@gmail.com>
2022-03-25 07:55:38 -0700

    shshs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - entry point
+ *main - entry point
  *
- * Retrun: 0 aftern printing the function
- * /
+ *Retrun: 0 aftern printing the function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
