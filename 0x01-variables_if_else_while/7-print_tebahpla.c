3d91dcb1e57a67397ecd0c2a2fc14483fa32cc49
root <selehadinseid1@gmail.com>
2022-03-26 07:56:32 -0700

    shshshh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success\correct)
+ */
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar (alphabet);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
