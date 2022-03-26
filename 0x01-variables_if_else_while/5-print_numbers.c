478268b03c6e62ac1e34081b9bf02e4dd0eb1010
root <selehadinseid1@gmail.com>
2022-03-26 07:27:04 -0700

    shshsj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Seccess\correct)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	printf('\n');
+
+	return (0);
+
+}
+
