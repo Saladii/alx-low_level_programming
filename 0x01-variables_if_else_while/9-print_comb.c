59afee3f0a8bffda11d98fe084c34c50a7387609
root <selehadinseid1@gmail.com>
2022-03-26 08:25:24 -0700

    sshhs

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#incude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success\correct)
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	
+		if (n == 57)
+		{
+			break;
+		}
+
+		putchar(',');
+		putchar(' ');
+
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
