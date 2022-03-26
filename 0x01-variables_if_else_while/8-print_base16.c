f7361abb67b0cb1bebaa77180fe02b77abd63d1b
root <selehadinseid1@gmail.com>
2022-03-26 08:11:37 -0700

    ahaha

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success\correct)
+ */
+int main(void)
+
+{
+	char b;
+
+	for (b = 48; b <=57; b++)
+	{
+		putchar(b);
+	}
+
+	for (b = 97; b <= 102; b++)
+	{
+		putchar(b);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
