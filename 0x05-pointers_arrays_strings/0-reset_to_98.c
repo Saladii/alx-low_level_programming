8a430e91611b8238103ff816b68abfa4b552adce
root <selehadinseid1@gmail.com>
2022-03-27 05:24:11 -0700

    ahsjasha

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**reset_to_98 - update an integer value to 98
+/**
+ * reset_to_98 - updates an integer value to 98
  * @n: Apointer to an int that will be updated
  *
  * Return: 0
