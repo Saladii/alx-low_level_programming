dd4c94e2c0b688679b9f321b488b3aeeb37cd1dd
root <selehadinseid1@gmail.com>
2022-03-27 05:17:39 -0700

    jajja

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**reset_to_98 - update an integer value to 98
+ * @n: Apointer to an int that will be updated
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
+
+{
+	*n = 98;
+}
