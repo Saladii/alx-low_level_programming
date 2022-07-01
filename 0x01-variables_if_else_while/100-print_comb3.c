cb0f055d4fd32793e51103b2673205ce2b7bf08e
Selehadin <selehadinseid1@gmail.com>
2022-07-01 18:41:20 +0300

    hjghgj

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i == 8 && j == 9)
+				{
+					putchar(10);
+				}
+				else
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	return (0);
+}
