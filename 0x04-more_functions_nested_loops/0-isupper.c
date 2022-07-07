1ac51be6b593b011381866d3ff582847864a313f
Selehadin <selehadinseid1@gmail.com>
2022-07-07 17:13:57 +0300

    jkvcx

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+    c = 'A';
+    c = 'a';
+if (c == 'A')
+{
+    printf("%c: %d\n", c, '1');
+}else
+{
+    printf("%c: %d\n", c, '0');
+}
+
+}
