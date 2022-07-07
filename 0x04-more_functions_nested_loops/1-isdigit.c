f35092b92bf7ac0b516443613c5a9d7da85d77b9
Selehadin <selehadinseid1@gmail.com>
2022-07-07 17:50:55 +0300

    kjsdsd

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigi - checks for digit
+ * @c: character to be cheked
+ * Return - 1 or 0
+ */
+ int _isdigit(int c)
+ {
+    if (c >= 48 && c <= 57)
+    {
+return ('1');
+    }
+    else
+    {
+return ('0');
+    }
+ }
+ 
