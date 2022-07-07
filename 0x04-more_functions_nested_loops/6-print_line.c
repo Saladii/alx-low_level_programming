5de0e8b11c57d0dc085e4263a4dfff658c35371b
Selehadin <selehadinseid1@gmail.com>
2022-07-07 18:47:45 +0300

    jsdd

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,8 +9,9 @@ void print_line(int n)
 {
     if (n > 0)
     {
+        int i;
         
-        for ( int i = 0; i < n; i++)
+        for (i = 0; i < n; i++)
         {
             _putchar('_');
         }
