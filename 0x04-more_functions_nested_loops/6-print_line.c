141855c7acac7b4349c92f82b13122f76ca6211e
Selehadin <selehadinseid1@gmail.com>
2022-07-07 18:49:19 +0300

    dnfmf

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * print_line - print a stright line
- * @c: character to be printed
- * Return - a line
+ * print_line - function that draws a straight
+ * line in the terminal.
+ * @n: length of line
  */
+
 void print_line(int n)
 {
-    if (n > 0)
-    {
-        int i;
-        
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');
-        }
-         
-    }
-    else {
-        _putchar('\n');
-    }
+	int i;
 
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
 }
