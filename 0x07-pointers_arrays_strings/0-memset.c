f0755dc8bf71b200918eec2b09bae8c29a55793b
Selehadin <selehadinseid1@gmail.com>
2022-07-18 18:24:05 +0300

    ksjd

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main
+ * 
+ * Return
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        s = b;
+    }
+    return (s);
+}
