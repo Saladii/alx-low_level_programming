ce507762e73118e8b346462410664539f924e300
Selehadin <selehadinseid1@gmail.com>
2022-07-19 14:56:40 +0300

    utu

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,14 @@
 
 /**
  * _memset - pinter function
- * 
+ *
  * Return - s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
-
+    
     for (i = 0; i < n; i++)
     {
         *(s + i) = b;
