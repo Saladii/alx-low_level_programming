ab64f678d98aaf9d4d247088ef576f299cfb250f
Selehadin <selehadinseid1@gmail.com>
2022-07-21 20:43:21 +0300

    lksjhd

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion -function that reverse a string
+ * @s: a string
+ * 
+ * Return - nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        _print_rev_recursion(s + 1);
+        _putchar(*s);
+    }
+    
+}
