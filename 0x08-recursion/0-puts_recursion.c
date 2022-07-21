fbf33ce6eff25e88eb6786ea24fdf57ff15fa44a
Selehadin <selehadinseid1@gmail.com>
2022-07-21 19:12:07 +0300

    kjhsdhjs

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-	i = 0;
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		putchar(*s);
-	}
-
 	putchar('\n');
+	return;	
+	}
+	putchar(*s);
+	_puts_recursion(*(s + 1));
 }
