92f76bd7742fbde9d4d8bbfd1d315b703c955510
Selehadin <selehadinseid1@gmail.com>
2022-07-21 20:51:54 +0300

    lksjd

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,16 +4,15 @@
 /**
  * _print_rev_recursion -function that reverse a string
  * @s: a string
- * 
+ *
  * Return - nothing
  */
 
 void _print_rev_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _print_rev_recursion(s + 1);
-        putchar(*s);
-    }
-    
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+putchar(*s);
+} 
 }
