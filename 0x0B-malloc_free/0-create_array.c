24051a5cb84bc5c687e4b21cfd71050d7b718c40
Selehadin <selehadinseid1@gmail.com>
2022-07-26 19:32:10 +0300

    jsdh

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,30 +6,30 @@
  * *create_array - array function
  * @size: size of the array
  * @c: character
- * 
+ *
  * Return - a pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *l;
-    l = malloc(size * sizeof(char));
+unsigned int i;
+char *l;
+l = malloc(size * sizeof(char));
 if (l == NULL)
 {
-    putchar('\n');
-    return(NULL);
+putchar('\n');
+return(NULL);
 }
 for (i = 0; i < size; i++)
 {
-    *(l + i) = c;
+*(l + i) = c;
 }
 if (size == 0)
 {
-    return(NULL);
+return(NULL);
 }
 else
 {
-    return(l);
+return(l);
 }
 }
