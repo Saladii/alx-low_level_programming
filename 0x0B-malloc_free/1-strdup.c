5e22737bb7fe7e458a7ad6faeba9d31355fba806
Selehadin <selehadinseid1@gmail.com>
2022-07-26 20:50:35 +0300

    lkjs

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a pointer function
+ * @str: a string argumment
+ * 
+ * Return: a string as a pointer
+ */
+char *_strdup(char *str)
+{
+    int i;
+    int length;
+    char l;
+    int x;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+    }
+    length = i - 1;
+    l = malloc(length * sizeof(char));
+    for (x = 0; x < length; x++)
+    {
+        l[x] = str[x];
+    }  
+}
