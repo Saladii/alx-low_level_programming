930187f6bda83fa8029401fb0821c630c8f25624
Selehadin <selehadinseid1@gmail.com>
2022-07-28 14:31:41 +0300

    ghf

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,53 +4,53 @@
 /**
  * _strdup - a pointer function
  * @str: a string argumment
- * 
+ *
  * Return: a string as a pointer
  */
 char *_strdup(char *str)
 {
-    int i;
-    int length;
-    char *l;
-    int x;
-    int y;
+int i;
+int length;
+char *l;
+int x;
+int y;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; str[i] != '\0'; i++)
-    {
-    }
-    length = i - 1;
-    y = i + 1;
-    if (length > 0)
-    {
-    l = malloc(y * sizeof(char));
-    if (l == NULL)
-    {
-        return (NULL);/* code */
-    }
-    else
-    {
-    for (x = 0; x <= length; x++)
-    {
-        l[x] = str[x];
-    } 
-    return (l);
-    }
-    }
-    else
-    {
-        l = malloc(1 * sizeof(char));
-        if (l == NULL)
-    {
-        return (NULL);/* code */
-    }
-    else {    
-        l[0] = str[0];
-        return (l);
-    }
-    }
-    free(l);
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+length = i - 1;
+y = i + 1;
+if (length > 0)
+{
+l = malloc(y * sizeof(char));
+if (l == NULL)
+{
+return (NULL);/* code */
+}
+else
+{
+for (x = 0; x <= length; x++)
+{
+l[x] = str[x];
+} 
+return (l);
+}
+}
+else
+{
+l = malloc(1 * sizeof(char));
+if (l == NULL)
+{
+return (NULL);/* code */
+}
+else {    
+l[0] = str[0];
+return (l);
+}
+}
+free(l);
 }
