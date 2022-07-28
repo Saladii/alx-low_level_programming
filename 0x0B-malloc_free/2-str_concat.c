e5b4ca49a9035c5301109b8d36d5e145ef0f8367
Selehadin <selehadinseid1@gmail.com>
2022-07-28 19:02:51 +0300

    jh

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,13 @@
 char *str_concat(char *s1, char *s2)
 {
 if (s1 == NULL)
+{
 s1 = "";
-if (s2 == NULL)
-s2 = "";    
+}
+else (s2 == NULL)
+{
+s2 = ""; 
+}   
 int i = strlen(s1);
 int y = strlen(s2);
 int c;
