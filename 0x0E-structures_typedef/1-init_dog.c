f3d2707516841801d52b6e774e6e02780b5c19b0
Selehadin <selehadinseid1@gmail.com>
2022-08-01 18:41:25 +0300

    wew

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,8 @@
  * Return: no return.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	
+{	
+d->name = name;
+d->age = age;
+d->owner = owner;	
 }
